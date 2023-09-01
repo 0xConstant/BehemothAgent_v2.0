418f95e18dbb930f1fd680e892f47e803072dc9e
0xConstant <noroh4xy@protonmail.com>
2023-09-01 11:37:07 -0700

    dictionary merger was just added

--- BehemothAgent_v2.0/main.cpp
+++ BehemothAgent_v2.0/main.cpp
@@ -9,6 +9,7 @@
 #include "self_dest.h"
 #include "system_id.h"
 #include "profiler.h"
+#include "merge_dics.h"
 
 
 /*
@@ -18,7 +19,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 */
 
 int main() {
-    std::cout << profiler() << std::endl;
+    std::unordered_map<std::string, int> dict1 = { {"a", 1}, {"b", 2} };
+    std::unordered_map<std::string, int> dict2 = { {"b", 3}, {"c", 4} };
+    std::vector<std::unordered_map<std::string, int>> dicts = { dict1, dict2 };
+    auto mergedDict = merge_dics(dicts);
+    for (const auto& kvp : mergedDict) {
+        std::cout << kvp.first << ": " << kvp.second << std::endl;
+    }
+
     return 0;
 }
 
